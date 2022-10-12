3f6b34bcf139c4405beb164639969c6e3bf5aea2
David Amorim <dmaa.costa@campus.fct.unl.pt>
2022-10-12 14:01:53 +0100

    Fix token problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,25 @@ import java.io.FileInputStream
 plugins {
     kotlin("jvm") version "1.6.20" apply false
     // https://github.com/d-costa/sessionkotlin/packages/1416283
-    id("com.github.d-costa.sessionkotlin.plugin") version "2.0.2" apply false
+    id("com.github.d-costa.sessionkotlin.plugin") version "2.0.3" apply false
 }
 
 subprojects{
     repositories {
-        mavenCentral()
         maven {
-            val skGithubPackagesRepo: String by project
-            name = skGithubPackagesRepo
+            // Load GitHub credentials
+            val props = Properties()
+            val envFile = File(rootDir.path + "/.env")
+            if (envFile.exists())
+                props.load(FileInputStream(envFile))
+
+            name = "GithubPackages"
             url = uri("https://maven.pkg.github.com/d-costa/sessionkotlin")
+            credentials {
+                username = props.getProperty("USERNAME") ?: System.getenv("USERNAME")
+                password = props.getProperty("TOKEN") ?: System.getenv("TOKEN")
+            }
         }
+        mavenCentral()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,21 @@
 pluginManagement {
     repositories {
+        maven {
+            // Load GitHub credentials
+            val props = java.util.Properties()
+            val envFile = File(rootDir.path + "/.env")
+            if (envFile.exists())
+                props.load(java.io.FileInputStream(envFile))
+
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/d-costa/sessionkotlin")
+            credentials {
+                username = props.getProperty("USERNAME") ?: System.getenv("USERNAME")
+                password = props.getProperty("TOKEN") ?: System.getenv("TOKEN")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
-
-        maven {
-            name = "SessionKotlin-GithubPackages"
-            url = uri("https://maven.pkg.github.com/d-costa/sessionkotlin")
-        }
     }
 }
 
